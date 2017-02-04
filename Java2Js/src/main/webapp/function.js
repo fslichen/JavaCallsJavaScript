aed09360a13ea292dba529cc062886ae45b0a789
user <user@use1r-PC>
2017-02-04 11:21:06 +0800

    Update

--- Java2Js/src/main/webapp/function.js
+++ Java2Js/src/main/webapp/function.js
@@ -1,3 +1,19 @@
 function anyFunction(string) {
 	return string;
 }
+
+function arrayOrListFunction(arrayOrList) {// Accept an array like int[] and list.
+	var string = "";
+	for (var i = 0; i < arrayOrList.length; i++) {
+		string += arrayOrList[i] + "\n";
+	}
+	return string;
+}
+
+function mapFunction(map) {// Accept a map.
+	var string = "";
+	for (var key in map) {
+		string += key + " : " + map[key] + "\n";
+	}
+	return string;
+}
--- Java2Js/src/test/java/evolution/JsUtilTest.java
+++ Java2Js/src/test/java/evolution/JsUtilTest.java
@@ -1,18 +1,48 @@
 package evolution;
+import java.util.LinkedHashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeSet;
+
 import org.junit.Test;
 
-import evolution.JsUtil;
-
 public class JsUtilTest {
-	@Test
+//	@Test
 	public void testFunction() {
 		Object object = JsUtil.function("http://localhost:8080/function.js", "anyFunction", "Hello World");
 		System.out.println(object);
 	}
 	
-	@Test
+//	@Test
 	public void testVariableFunction() {
 		Object object = JsUtil.variableFunction("http://localhost:8080/variableFunction.js", "anyVariable", "anyFunction", "Hello World");
+		System.out.println(object);
+	}
+	
+//	@Test
+	public void testArrayFunction() {
+		int[] array = {1, 2, 3};
+		Object object = JsUtil.function("http://localhost:8080/function.js", "arrayOrListFunction", array);
+		System.out.println(object);
+	}
+	
+//	@Test
+	public void testMapFunction() {
+		Map<Integer, Double> map = new LinkedHashMap<>();
+		map.put(1, 3.14);
+		map.put(2, 6.28);
+		Object object = JsUtil.function("http://localhost:8080/function.js", "mapFunction", map);
+		System.out.println(object);
+	}
+	
+	@Test
+	public void testListFunction() {
+		List<String> list = new LinkedList<>();
+		list.add("Hello World");
+		list.add("Goodbye Past");
+		Object object = JsUtil.function("http://localhost:8080/function.js", "arrayOrListFunction", list);
 		System.out.println(object);
 	}
 }
