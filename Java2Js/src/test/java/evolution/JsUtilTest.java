f87587b59329bef95fd8d6c2976bbb0991a35ccc
user <user@use1r-PC>
2017-02-04 11:21:57 +0800

    Update

--- Java2Js/src/test/java/evolution/JsUtilTest.java
+++ Java2Js/src/test/java/evolution/JsUtilTest.java
@@ -3,8 +3,6 @@ import java.util.LinkedHashMap;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
-import java.util.TreeSet;
 
 import org.junit.Test;
 
